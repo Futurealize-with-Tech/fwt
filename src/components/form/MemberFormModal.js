3a7d44615983c4c203b5a7e567cb6a4c7077e2f0
Nozomi Fukawa <noppi5959@gmail.com>
2024-01-08 23:40:44 +0900

    fix: fix-form-layout #29

--- src/components/form/MemberFormModal.js
+++ src/components/form/MemberFormModal.js
@@ -2,6 +2,7 @@ import styles from "./memberform.module.scss";
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../../utils/supabase";
 import { postMessage } from "../../../utils/supabasePostFunctions";
+import { HiArrowCircleRight } from "react-icons/hi";
 
 export const MemberFormModal = ({ buttonLabel }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,46 +28,57 @@ export const MemberFormModal = ({ buttonLabel }) => {
       <button onClick={toggleModal}>{buttonLabel}</button>
       {isOpen && (
         <div className={styles["overly"]}>
-          <form onSubmit={handleSubmit} className={styles["content"]}>
-            <div>
-              <p>TO</p>
-              <p>(メンター名)</p>
-              <div className={styles["container"]}>
-                <p>FROM</p>
-                <input
-                  type="text"
-                  className={styles["name-textform"]}
-                  placeholder="メンバー名を入力"
-                  value={memberName}
-                  onChange={(e) => setmemberName(e.target.value)}
-                />
-              </div>
-              <div className={styles["container"]}>
-                <p>MESSAGE</p>
-                <textarea
-                  name="message"
-                  cols="30"
-                  rows="10"
-                  className={styles["message-textform"]}
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                ></textarea>
-              </div>
-              {/* <input type="submit" value="送信" /> */}
-              <input
-                className={styles["toImgBtn"]}
-                value="画像デザインを選択"
-              />
+          <div className={styles["content"]}>
+            <form onSubmit={handleSubmit}>
               <div>
-                <div
-                  onClick={() => setIsOpen(!isOpen)}
-                  className={styles["cancelBtn"]}
-                >
-                  キャンセル
+                <div className={styles["container"]}>
+                  <div>TO</div>
+                  <div>(メンター名)</div>
+                </div>
+                <div className={styles["container"]}>
+                  <div>FROM</div>
+                  <input
+                    type="text"
+                    className={styles["name-textform"]}
+                    placeholder="メンバー名を入力"
+                    value={memberName}
+                    onChange={(e) => setmemberName(e.target.value)}
+                  />
+                </div>
+                <div className={styles["container"]}>
+                  <div>MESSAGE</div>
+                  <textarea
+                    name="message"
+                    cols="30"
+                    rows="10"
+                    className={styles["message-textform"]}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="メッセージを入力"
+                  ></textarea>
+                </div>
+                <div className={styles["submitBtn"]}>
+                  <div>
+                    <input
+                      className={styles["toImgBtn"]}
+                      value="画像デザインを選択"
+                    />
+                  </div>
+                  <div>
+                    <HiArrowCircleRight className={styles["icon"]} />
+                  </div>
+                </div>
+                <div>
+                  <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={styles["cancelBtn"]}
+                  >
+                    キャンセル
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       )}
     </>
--- utils/supabasePostFunctions.js
+++ utils/supabasePostFunctions.js
@@ -3,5 +3,10 @@ import { supabase } from "./supabase";
 export const postMessage = async (memberName, message) => {
   await supabase
     .from("message")
-    .insert({ member_name: memberName, body: message });
+    .insert({
+      member_name: memberName,
+      body: message,
+      mentor_id: 1,
+      card_design: "1",
+    });
 };
