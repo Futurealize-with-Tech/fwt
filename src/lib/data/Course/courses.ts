e90e6589ec0e7cb540ccd9027029e4f790a3c32b
HASURO <rentaro170928@icloud.com>
2024-01-10 18:53:28 +0900

    add: icon #39

--- src/lib/data/Course/courses.ts
+++ src/lib/data/Course/courses.ts
@@ -3,6 +3,13 @@ import AndroidImage from '@/public/course/android.webp';
 import WebDImage from '@/public/course/webd.webp';
 import WebSImage from '@/public/course/webs.webp';
 import UnityImage from '@/public/course/unity.webp';
+import MinecraftImage from '@/public/course/minecraft-icon.png';
+import MovieImage from '@/public/course/movie.webp';
+import AnimationImage from '@/public/course/animation-icon.png';
+import DesignerImage from '@/public/course/designer.png';
+import MediaArtImage from '@/public/course/media-art-icon.png';
+import DTMImage from '@/public/course/dtm-icon.png';
+import MikuImage from '@/public/course/miku-icon.png';
 import { CourseType } from '@/types/courseType';
 
 export const courses = <CourseType[]>[
@@ -12,11 +19,11 @@ export const courses = <CourseType[]>[
     {name: 'webs', nameText: 'WebS', icon: WebSImage},
     {name: 'webd', nameText: 'WebD', icon: WebDImage},
     {name: 'unity', nameText: 'Unity', icon: UnityImage},
-    {name: 'minecraft', nameText: 'マイクラ'},
-    {name: 'eizou', nameText: '映像'},
-    {name: 'animation', nameText: 'アニメーション'},
-    {name: 'designer', nameText: 'デザイナー'},
-    {name: 'mediaart', nameText: 'メディアアート'},
-    {name: 'dtm', nameText: 'DTM'},
-    {name: 'miku', nameText: '初音ミク'},
+    {name: 'minecraft', nameText: 'マイクラ', icon: MinecraftImage},
+    {name: 'eizou', nameText: '映像', icon: MovieImage},
+    {name: 'animation', nameText: 'アニメーション', icon: AnimationImage},
+    {name: 'designer', nameText: 'デザイナー', icon: DesignerImage},
+    {name: 'mediaart', nameText: 'メディアアート', icon: MediaArtImage},
+    {name: 'dtm', nameText: 'DTM', icon: DTMImage},
+    {name: 'miku', nameText: '初音ミク', icon: MikuImage},
 ];
