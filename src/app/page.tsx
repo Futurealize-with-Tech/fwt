dfd75a300f861343d3c8c77fea7460cf36a28b79
Nozomi Fukawa <noppi5959@gmail.com>
2024-01-19 22:08:52 +0900

    create-post-api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import styles from "./page.module.scss";
 import MentorIndex from "@/components/Home/MentorIndex";
 
-const getMentorList = async () => {
-  const res = await fetch("http://localhost:3000/api/v1/mentors");
-  const json = await res.json();
-  return json.mentors;
-};
-
 export default function Home() {
   return (
     <div className={styles["container"]}>
