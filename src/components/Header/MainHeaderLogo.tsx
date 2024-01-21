167e8922049fc20de6304edc1efbbbf5777ec96a
HASURO <rentaro170928@icloud.com>
2024-01-21 18:14:00 +0900

    change: divide file #88

---
+++ src/components/Header/MainHeaderLogo.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import styles from './header.module.scss';
+import Image from 'next/image';
+import TitleLogoImage from '@/public/app/fwt-title.png';
+import LogoImage from '@/public/app/fwt-logo-white.png';
+
+export default function MainHeaderLogo() {
+    return (
+        <>
+        <Image src={TitleLogoImage} alt='Featureaize with Tech！' width={200} className={styles['title-logo-image']} />
+        <Image src={LogoImage} alt='Featureaize with Tech！' width={200} className={styles['logo-image']} />
+        </>
+    );
+};
