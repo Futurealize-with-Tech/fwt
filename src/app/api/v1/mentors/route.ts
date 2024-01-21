f428c3349a48c98df74aa7cb0f4d15087651da4f
HASURO <rentaro170928@icloud.com>
2024-01-21 20:29:54 +0900

    hotfix: re update #97

--- src/app/api/v1/mentors/route.ts
+++ src/app/api/v1/mentors/route.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from "next/server";
 import { prisma, prismaConnect } from "@/lib/prisma/prisma";
 
-
 export const GET = async (req: Request, res: NextResponse) => {
-    return NextResponse.json({ mentors }, { status: 200 });
-  } catch (e) {
-    return NextResponse.json({ message: "fetch error", e }, { status: 500 });
-  } finally {
-    prisma.$disconnect();
-  }
+    try {
+        await prismaConnect();
+        const mentors = await prisma.mentor.findMany({
+            include: {
+                regions: true,
+                courses: true,
+            },
+        });
+
+        return NextResponse.json({mentors}, {status: 200});
+    } catch (e) {
+        return NextResponse.json({message: 'fetch error', e}, {status: 500});
+    } finally {
+        prisma.$disconnect();
+    };
 };
