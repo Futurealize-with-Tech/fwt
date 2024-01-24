a4cacc4c8c86f43739a90016b07d942dbb8c8f2e
HASURO <rentaro170928@icloud.com>
2024-01-24 21:55:42 +0900

    add: cardDesigns data #64

---
+++ src/lib/data/CardDesign/cardDesigns.ts
@@ -0,0 +1,8 @@
+import { CardDesignType } from "@/types/cardDesignType";
+import CampImage from '@/public/img/image1.jpg';
+import SchoolImage from '@/public/img/image2.jpg';
+
+export const cardDesigns = <CardDesignType[]>[
+    {id: 0, name: "認定証【キャンプ】", image: CampImage},
+    {id: 1, name: "認定証【スクール】", image: SchoolImage},
+];
