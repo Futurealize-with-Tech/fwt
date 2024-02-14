4ee2519b0232cb7d890a9267c8d63ce4c86a640d
futurealizeWithTech <159965730+futurealizeWithTech@users.noreply.github.com>
2024-02-14 15:30:14 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import styles from './page.module.scss';
 import MentorIndex from '@/components/Home/MentorIndex';
 import MentorsDataProvider from '@/middleware/MentorsDataProvider';
 import { MentorType } from '@/types/mentorType';
+import { mentors } from '@/lib/data/mentors.ts'
 
-async function getMentorsData() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/mentors`);
+// async function getMentorsData() {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/mentors`);
 
-    const data = await res.json();
-    return data.mentors as MentorType[];
-}
+//     const data = await res.json();
+//     return data.mentors as MentorType[];
+// }
 
 export default async function Home() {
-    const mentorsData = await getMentorsData();
+    // const mentorsData = await getMentorsData();
+    const mentorsData = mentorsData();
 
     return (
         <div className={styles['container']}>
