64abb1b68d855e0c236f67769e652f404f0c4e9c
HASURO <rentaro170928@icloud.com>
2024-02-14 15:41:38 +0900

    remove: fetch #126

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import styles from './page.module.scss';
 import MentorIndex from '@/components/Home/MentorIndex';
+import { mentors } from '@/lib/data/mentors';
 import MentorsDataProvider from '@/middleware/MentorsDataProvider';
 import { MentorType } from '@/types/mentorType';
 
-async function getMentorsData() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/mentors`);
+// async function getMentorsData() {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/mentors`);
 
-    const data = await res.json();
-    return data.mentors as MentorType[];
-}
+//     const data = await res.json();
+//     return data.mentors as MentorType[];
+// }
 
 export default async function Home() {
-    const mentorsData = await getMentorsData();
+    // const mentorsData = await getMentorsData();
+    const mentorsData = mentors;
 
     return (
         <div className={styles['container']}>
